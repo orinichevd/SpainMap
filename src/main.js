891d904b00e23c93c9e9c08b94fb4d3ad157b819
orinichev <orinichev@hotmail.com>
2019-09-20 22:10:04 +0300

    add three geo lib

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const tgeo = new ThreeGeo({
+    tokenMapbox: 'pk.eyJ1Ijoib3JpbmljaGV2IiwiYSI6ImNrMHNodzY1ODAybHkzZXFyZ253YmFxZWIifQ.beBMOwb9cT7O_rCMUxjB2Q',                
+});
+
+var mapController = {
+    lat: 41.9802833
+    , lon: 2.8011577
+    , r: 50.0 //km
+}
+
